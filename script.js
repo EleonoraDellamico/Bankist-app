d8311b9ab188bb6d924bdb1033c75e9646f8bc56
Uli Dell’Amico <ulidellamico@MacBook-Air-de-Uli.local>
2021-05-10 10:52:44 +0200

    added function displayMovements with a forEach method and add to the html

--- script.js
+++ script.js
@@ -4,36 +4,36 @@
 /////////////////////////////////////////////////
 // BANKIST APP
 
-// Data
+// Data simulation API JSON
 const account1 = {
-  owner: 'Jonas Schmedtmann',
-  movements: [200, 450, -400, 3000, -650, -130, 70, 1300],
-  interestRate: 1.2, // %
-  pin: 1111,
+	owner: 'Jonas Schmedtmann',
+	movements: [ 200, 450, -400, 3000, -650, -130, 70, 1300 ],
+	interestRate: 1.2, // %
+	pin: 1111
 };
 
 const account2 = {
-  owner: 'Jessica Davis',
-  movements: [5000, 3400, -150, -790, -3210, -1000, 8500, -30],
-  interestRate: 1.5,
-  pin: 2222,
+	owner: 'Jessica Davis',
+	movements: [ 5000, 3400, -150, -790, -3210, -1000, 8500, -30 ],
+	interestRate: 1.5,
+	pin: 2222
 };
 
 const account3 = {
-  owner: 'Steven Thomas Williams',
-  movements: [200, -200, 340, -300, -20, 50, 400, -460],
-  interestRate: 0.7,
-  pin: 3333,
+	owner: 'Steven Thomas Williams',
+	movements: [ 200, -200, 340, -300, -20, 50, 400, -460 ],
+	interestRate: 0.7,
+	pin: 3333
 };
 
 const account4 = {
-  owner: 'Sarah Smith',
-  movements: [430, 1000, 700, 50, 90],
-  interestRate: 1,
-  pin: 4444,
+	owner: 'Sarah Smith',
+	movements: [ 430, 1000, 700, 50, 90 ],
+	interestRate: 1,
+	pin: 4444
 };
 
-const accounts = [account1, account2, account3, account4];
+const accounts = [ account1, account2, account3, account4 ];
 
 // Elements
 const labelWelcome = document.querySelector('.welcome');
@@ -61,16 +61,33 @@ const inputLoanAmount = document.querySelector('.form__input--loan-amount');
 const inputCloseUsername = document.querySelector('.form__input--user');
 const inputClosePin = document.querySelector('.form__input--pin');
 
+////////////////////////////////////////////////////////////////
+//we passs parametres to the function better then a global scope
+const displayMovements = function(movements) {
+	//to cut html
+	containerMovements.innerHTML = '';
+	//add new part in the html
+	movements.forEach(function(mov, i) {
+		const type = mov > 0 ? 'deposit' : 'withdrawal';
+
+		const html = `
+    <div class="movements__row">
+          <div class="movements__type movements__type--${type}">${i + 1} ${type}</div>
+          <div class="movements__value">${mov}</div>
+        </div>
+    `;
+		//use insertAdjacenthtml to insert to the html--this method recive 2 parametres 1 the position 2 the string we want to add in the html.
+		containerMovements.insertAdjacentHTML('afterbegin', html);
+	});
+};
+displayMovements(account1.movements);
+//console.log(containerMovements.innerHTML);
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
 // LECTURES
 
-const currencies = new Map([
-  ['USD', 'United States dollar'],
-  ['EUR', 'Euro'],
-  ['GBP', 'Pound sterling'],
-]);
+const currencies = new Map([ [ 'USD', 'United States dollar' ], [ 'EUR', 'Euro' ], [ 'GBP', 'Pound sterling' ] ]);
 
-const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+const movements = [ 200, 450, -400, 3000, -650, -130, 70, 1300 ];
 
 /////////////////////////////////////////////////
